9e5815912be3efe32957fe194aec3334342b5b6e
Nadir Alkan <nadir.alkan@aurea.com>
2019-07-18 09:06:17 +0300

    Cloned repo-first commit

---
+++ new-repo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+repo="$1" 
+
+rm -rf .git 
+git init 
+git config user.email "nadir.alkan@aurea.com" 
+git config user.name "Nadir Alkan" 
+git add -A 
+git commit -m "Cloned repo-first commit" 
+
+git remote add origin github.com-aurea:nadir-alkan-aurea/$repo.git
+git push -u origin master
+
+echo "Completed as $repo" 
